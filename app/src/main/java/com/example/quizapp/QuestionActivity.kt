c5f6fb103322e15ca6de495bb3da19eb5179ea7e
holutunde <holutunde99@gmail.com>
2021-09-07 15:26:22 +0100

    adjust view

--- app/src/main/java/com/example/quizapp/QuestionActivity.kt
+++ app/src/main/java/com/example/quizapp/QuestionActivity.kt
@@ -3,9 +3,7 @@ package com.example.quizapp
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
-import android.media.Image
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
